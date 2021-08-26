8876d6c19b566730eedb7747b4a9883db00dfb10
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-08-26 16:59:05 +0000

    ExerFor2

---
+++ ExerFor2.java
@@ -0,0 +1,9 @@
+public class ExerFor2 {
+    public static void main(String[] args) {
+        int z=0;
+        for(int n=1;n<=1000;n++){
+            z+=n;
+        }
+        System.out.println("Soma de 1 até 1000: "+z);
+    }
+}
